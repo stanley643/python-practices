b206ef749e6a74a38e8537db49891ba02d4da424
Stanley Murigi Njoroge <98167574+stanley643@users.noreply.github.com>
2022-04-21 23:04:04 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number: 
+        guess =int(input(f'Guess a number btw 1 and {x}: ')) 
+        if guess< random_number:
+            print('Sorry ,guess again. Too low')
+        elif guess> random_number:
+            print('Sorry, guess again. Too High')
+
+    print(f'yay, congrants. the number is {random_number}')
+
+def comp_guess(x):
+        low = 1
+        high= x
+        feedback = ''
+        while feedback != 'c':
+            if low != high:
+              guess = random.randint(low, high)
+            else:
+                guess = low
+            feedback = input(f'is{guess} too high (H), too low (L), or correct(C) ??').lower
+            if feedback == 'h':
+                high = guess-1
+            elif feedback == 'l':
+                low = guess+1
+        print(f'yay! The computer guessed your number,{guess} , correctly')
+comp_guess(10)
